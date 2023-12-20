5d504a965ba229acb064afe0166a55c790da00c9
Adaobi13 <154443186+Adaobi13@users.noreply.github.com>
2023-12-20 06:28:56 -0800

    Add files via upload

---
+++ CircleTheorem.js
@@ -0,0 +1,504 @@
+// Get the input element with the ID "aa"
+const aa = document.getElementById("aa");
+
+// Add an input event listener to the input element
+aa.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = aa.value.trim();// Get input value and trim whitespace
+    const answer = "30";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    aa.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "ab"
+const ab = document.getElementById("ab");
+
+// Add an input event listener to the input element
+ab.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = ab.value.trim();// Get input value and trim whitespace
+    const answer = "15";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    ab.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "ac"
+const ac = document.getElementById("ac");
+
+// Add an input event listener to the input element
+ac.addEventListener("input", function() {
+  const ans = "2";
+  if (ac.value !== ans) {
+    ac.value = "";
+    console.log("Input cleared because it's not'2'");
+}
+});
+// Get the input element with the ID "ad"
+const ad = document.getElementById("ad");
+
+// Add an input event listener to the input element
+ad.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = ad.value.trim();// Get input value and trim whitespace
+    const answer = "45";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    ad.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "ae"
+const ae = document.getElementById("ae");
+
+// Add an input event listener to the input element
+ae.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = ae.value.trim();// Get input value and trim whitespace
+    const answer = "45";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    ae.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "af"
+const af = document.getElementById("af");
+
+// Add an input event listener to the input element
+af.addEventListener("input", function() {
+  const ans = "2";
+  if (af.value !== ans) {
+    af.value = "";
+    console.log("Input cleared because it's not'2'");
+}
+});
+// Get the input element with the ID "ai"
+const ai = document.getElementById("ai");
+
+// Add an input event listener to the input element
+ai.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = ai.value.trim();// Get input value and trim whitespace
+    const answer = "30";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    ai.value = "";
+}
+}, 2000);
+});
+// Get the input element with the ID "ag"
+const ag = document.getElementById("ad");
+
+// Add an input event listener to the input element
+ag.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = ag.value.trim();// Get input value and trim whitespace
+    const answer = "45";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    ag.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "ah"
+const ah = document.getElementById("ah");
+
+// Add an input event listener to the input element
+ah.addEventListener("input", function() {
+  const ans = "2";
+  if (ah.value !== ans) {
+    ah.value = "";
+    console.log("Input cleared because it's not'2'");
+}
+});
+// Get the input element with the ID "aj"
+const aj = document.getElementById("aj");
+
+// Add an input event listener to the input element
+aj.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = ag.value.trim();// Get input value and trim whitespace
+    const answer = "15";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    aj.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "twa"
+const twa = document.getElementById("twa");
+
+// Add an input event listener to the input element
+twa.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = twa.value.trim();// Get input value and trim whitespace
+    const answer = "33";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    twa.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "twb"
+const twb = document.getElementById("twb");
+
+// Add an input event listener to the input element
+twb.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = twb.value.trim();// Get input value and trim whitespace
+    const answer = "40";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    twb.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "twc"
+const twc = document.getElementById("twc");
+
+// Add an input event listener to the input element
+twc.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = twc.value.trim();// Get input value and trim whitespace
+    const answer = "33";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    twc.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "twd"
+const twd = document.getElementById("twd");
+
+// Add an input event listener to the input element
+twd.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = twd.value.trim();// Get input value and trim whitespace
+    const answer = "40";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    twd.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "twe"
+const twe = document.getElementById("twe");
+
+// Add an input event listener to the input element
+twe.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = twe.value.trim();// Get input value and trim whitespace
+    const answer = "28";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    twe.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "tha"
+const tha = document.getElementById("tha");
+
+// Add an input event listener to the input element
+tha.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = tha.value.trim();// Get input value and trim whitespace
+    const answer = "40";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    tha.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "thb"
+const thb = document.getElementById("thb");
+
+// Add an input event listener to the input element
+thb.addEventListener("input", function() {
+  const ans = "4";
+  if (thb.value !== ans) {
+    thb.value = "";
+    console.log("Input cleared because it's not'4'");
+}
+});
+// Get the input element with the ID "thc"
+const thc = document.getElementById("thc");
+
+// Add an input event listener to the input element
+thc.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = thc.value.trim();// Get input value and trim whitespace
+    const answer = "40";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    thc.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "thd"
+const thd = document.getElementById("thd");
+
+// Add an input event listener to the input element
+thd.addEventListener("input", function() {
+  const ans = "4";
+  if (thd.value !== ans) {
+    thd.value = "";
+    console.log("Input cleared because it's not'4'");
+}
+});
+// Get the input element with the ID "the"
+const the = document.getElementById("the");
+
+// Add an input event listener to the input element
+the.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = the.value.trim();// Get input value and trim whitespace
+    const answer = "40";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    the.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "thf"
+const thf = document.getElementById("thf");
+
+// Add an input event listener to the input element
+thf.addEventListener("input", function() {
+  const ans = "3";
+  if (thf.value !== ans) {
+    thf.value = "";
+    console.log("Input cleared because it's not'4'");
+}
+});
+// Get the input element with the ID "foa"
+const foa = document.getElementById("foa");
+
+// Add an input event listener to the input element
+foa.addEventListener("input", function() {
+  const ans = "b";
+  if (foa.value !== ans) {
+    foa.value = "";
+    console.log("Input cleared because it's not'b'");
+}
+});
+// Get the input element with the ID "fob"
+const fob = document.getElementById("fob");
+
+// Add an input event listener to the input element
+fob.addEventListener("input", function() {
+  const ans = "a";
+  if (fob.value !== ans) {
+    fob.value = "";
+    console.log("Input cleared because it's not'a'");
+}
+});
+// Get the input element with the ID "foh"
+const foh = document.getElementById("foh");
+
+// Add an input event listener to the input element
+foh.addEventListener("input", function() {
+  const ans = "a";
+  if (foh.value !== ans) {
+    foh.value = "";
+    console.log("Input cleared because it's not'a'");
+}
+});
+// Get the input element with the ID "foc"
+const foc = document.getElementById("foc");
+
+// Add an input event listener to the input element
+foc.addEventListener("input", function() {
+  const ans = "b";
+  if (foc.value !== ans) {
+    foc.value = "";
+    console.log("Input cleared because it's not'b'");
+}
+});
+// Get the input element with the ID "fod"
+const fod = document.getElementById("fod");
+
+// Add an input event listener to the input element
+fod.addEventListener("input", function() {
+  const ans = "c";
+  if (fod.value !== ans) {
+    fod.value = "";
+    console.log("Input cleared because it's not'c'");
+}
+});
+// Get the input element with the ID "foe"
+const foe = document.getElementById("foe");
+
+// Add an input event listener to the input element
+foe.addEventListener("input", function() {
+  const ans = "b";
+  if (foe.value !== ans) {
+    foe.value = "";
+    console.log("Input cleared because it's not'b'");
+}
+});
+// Get the input element with the ID "fof"
+const fof = document.getElementById("fof");
+
+// Add an input event listener to the input element
+fof.addEventListener("input", function() {
+  const ans = "c";
+  if (fof.value !== ans) {
+    fof.value = "";
+    console.log("Input cleared because it's not'c'");
+}
+});
+// Get the input element with the ID "fog"
+const fog = document.getElementById("fog");
+
+// Add an input event listener to the input element
+fog.addEventListener("input", function() {
+  const ans = "2";
+  if (fog.value !== ans) {
+    fog.value = "";
+    console.log("Input cleared because it's not'c'");
+}
+});
+// Get the input element with the ID "fiva"
+const fiva = document.getElementById("fiva");
+
+// Add an input event listener to the input element
+fiva.addEventListener("input", function() {
+  const ans = "1";
+  if (fiva.value !== ans) {
+    fiva.value = "";
+    console.log("Input cleared because it's not'1'");
+}
+});
+// Get the input element with the ID "fivb"
+const fivb = document.getElementById("fivb");
+
+// Add an input event listener to the input element
+fivb.addEventListener("input", function() {
+  const ans = "1";
+  if (fivb.value !== ans) {
+    fivb.value = "";
+    console.log("Input cleared because it's not'1'");
+}
+});
+// Get the input element with the ID "fivc"
+const fivc = document.getElementById("fivc");
+
+// Add an input event listener to the input element
+fivc.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = fivc.value.trim();// Get input value and trim whitespace
+    const answer = "180";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    fivc.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "fivd"
+const fivd = document.getElementById("fivd");
+
+// Add an input event listener to the input element
+fivd.addEventListener("input", function() {
+  clearTimeout();
+  timeOutId2 = setTimeout(() => {
+    const userInput = fivd.value.trim();// Get input value and trim whitespace
+    const answer = "180";
+    if (userInput === answer) {
+      console.log("Correct Answer");
+  }
+  else {
+    console.log("Incorrect Answer");
+    fivd.value = "";
+  }
+  }, 2000);
+});
+// Get the input element with the ID "five"
+const five = document.getElementById("five");
+
+// Add an input event listener to the input element
+five.addEventListener("input", function() {
+  const ans = "2";
+  if (five.value !== ans) {
+    five.value = "";
+    console.log("Input cleared because it's not'2'");
+}
+});
+// Get the input element with the ID "fivf"
+const fivf = document.getElementById("fivf");
+
+// Add an input event listener to the input element
+fivf.addEventListener("input", function() {
+  const ans = "2";
+  if (fivf.value !== ans) {
+    fivf.value = "";
+    console.log("Input cleared because it's not'2'");
+}
+});
